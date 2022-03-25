267194781e4e18de8421bffd985f5979d422ab7e
DennieCodes <oculareo@gmail.com>
2022-03-25 01:41:32 +0000

    created app skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GlobalStyles } from "./components/GlobalStyles";
+import styled, { ThemeProvider } from "styled-components";
 
+// Imported Components
+import Header from "./components/Header";
+
+// Styled Components
+const AppContainer = styled.div`
+`;
+
+const Main = styled.main`
+`;
+
+// Appliction Component
 function App() {
   return (
-    <main>
-      React⚛️ + Vite⚡ + Replit🌀
-    </main>
+    <>
+    <BrowserRouter>
+      <GlobalStyles/>
+      <AppContainer>
+        <Header />
+
+        <Main>
+          <Routes>
+            {/* <Route exact path="/local" element={<Test />}/> */}
+          </Routes>
+        </Main>
+      </AppContainer>
+    </BrowserRouter>
+    </>
   );
 }
 
